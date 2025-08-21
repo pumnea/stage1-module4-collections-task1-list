df709f399e249801d7d82a07d145aa856ac5600c
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-08-21 21:13:18 +0300

    add: ArrayListCreator implementation using stream api.

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -2,8 +2,15 @@ package com.epam.mjc.collections.list;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        return IntStream.range(0, sourceList.size())
+                .filter(idx -> (idx + 1) % 3 == 0)
+                .mapToObj(idx -> List.of(sourceList.get(idx), sourceList.get(idx)))
+                .flatMap(List::stream)
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 }
