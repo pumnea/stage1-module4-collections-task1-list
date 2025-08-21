140460c0dcd3c09113fd1971105a7dc4bad52df9
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-08-21 21:22:18 +0300

    add: LinkedListCreator implementation.

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> result = new LinkedList<>();
+        for (Integer num : sourceList) {
+            if (num % 2 == 0) {
+                result.addLast(num);
+            } else {
+                result.addFirst(num);
+            }
+        }
+        return result;
     }
 }
