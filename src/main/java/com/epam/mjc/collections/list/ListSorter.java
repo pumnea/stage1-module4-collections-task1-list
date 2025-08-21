070e1fe8a40e0a509a1e96946e620a0bfbf2c2c8
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-08-21 21:48:14 +0300

    add: ListSorter implementation.

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,24 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int first = Integer.parseInt(a);
+        int second = Integer.parseInt(b);
+        int firstResult = computeFunction(first);
+        int secondResult = computeFunction(second);
+        if (firstResult == secondResult) {
+            return Integer.compare(first, second);
+        }
+        return Integer.compare(firstResult, secondResult);
+    }
+
+    private Integer computeFunction(int x) {
+        return 5 * x * x + 3;
     }
 }
